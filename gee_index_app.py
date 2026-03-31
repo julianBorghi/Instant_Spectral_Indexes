9db210f4169d40e38ec494e757fb8ddfab3f1776
julianBorghi <116314676+julianBorghi@users.noreply.github.com>
2026-03-30 22:52:52 -0300

    Update gee_index_app.py

--- gee_index_app.py
+++ gee_index_app.py
@@ -60,7 +60,7 @@ with top_left:
             )
         
             # Initialize Earth Engine with service account
-            ee.Initialize(credentials, project=creds_info['project_id'])
+            ee.Initialize(credentials, project='don-carmelo-2025')
             ee.Authenticate()
             st.success("✅ Earth Engine connected via Service Account!")
         
